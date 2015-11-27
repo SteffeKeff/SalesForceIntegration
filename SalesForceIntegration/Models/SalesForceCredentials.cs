30e171f99896f56d57476680491892ba7ec60526
Stefan De Geer <sdgkeff@gmail.com>
2015-11-27 10:54:33 +0100

    I princip färdig integration till Salesforce

---
+++ SalesForceIntegration/Models/SalesForceCredentials.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesForceIntegration.Models
+{
+    public class SalesForceCredentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string SecurityToken { get; set; }
+    }
+}
